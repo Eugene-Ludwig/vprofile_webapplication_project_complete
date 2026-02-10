fda2938c2a41a7064bdb5e1126206d97c559c059
Eugene Ludwig <eugene.ludwig.113@gmail.com>
2026-02-10 21:09:58 +0000

    fixing another syntax error

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
 }
 
 output "nat_instance_public_ip" {
-  value       = module.fck-nat.public_ip
+  value       = module.fck-nat.instance_public_ip
   description = "The public IP of the fck-nat instance"
 }
 
