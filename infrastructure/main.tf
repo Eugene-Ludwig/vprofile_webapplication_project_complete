0539e2cfeb8a9e6b29e47452c07fcbf28b4011e3
Eugene Ludwig <eugene.ludwig.113@gmail.com>
2026-02-10 23:24:56 +0000

    fixing root main.tf file so it would apply route table to each sub-net

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,7 +24,8 @@ module "fck-nat" {
 }
 
 resource "aws_route" "private_nat_route" {
-  route_table_id         = module.vpc.private_route_table_ids[0]
+  count                  = length(module.vpc.private_route_table_ids)
+  route_table_id         = module.vpc.private_route_table_ids[count.index]
   destination_cidr_block = "0.0.0.0/0"
 
   network_interface_id = module.fck-nat.eni_id
